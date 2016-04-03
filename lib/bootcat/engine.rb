7e64b311b65fe45df457576fececa6c68c35dcbe
xiaohui <xiaohui@zhangxh.net>
2016-04-03 21:13:45 +0800

    fix i18n error

--- lib/bootcat/engine.rb
+++ lib/bootcat/engine.rb
@@ -2,6 +2,8 @@ module Bootcat
   class Engine < ::Rails::Engine
     # isolate_namespace Bootcat
     rake_tasks { load "tasks/bootcat_tasks.rake" }
+    require 'rails-i18n'
+    require 'i18n_yaml_generator'
     require 'ckeditor'
     require 'paperclip'
     require 'paperclip-qiniu'
